9232d706c8d8c1888fea7ce3cc74915aa5527efe
Andrr3wFs <andrew23_y@hotmail.com>
2026-08-11 16:09:11 -0500

    Conectar frontend con el backend en producción

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,12 @@
 // frontend/src/api.js
-
 import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://elegant-quietude-production-c1c5.up.railway.app', // o https://medicenters.uk
+  withCredentials: true
+});
+
+export default api;
 
 const NGROK_API_URL = 'https://tricrotic-noninhibitive-carlie.ngrok-free.dev/api';
 const DEFAULT_LOCAL_API_URL = 'http://127.0.0.1:5000';
